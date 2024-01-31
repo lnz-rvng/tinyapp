8c77fa75d6112d72aac65af00abe2546c21375d1
Lance <lanceirvingmanzanal@icloud.com>
2024-01-31 16:18:36 -0700

    Update the /urls/:id route

--- express_server.js
+++ express_server.js
@@ -32,7 +32,7 @@ app.get('/urls', (req, res) => {
 
 // Added a second route and template
 app.get('/urls/:id', (req, res) => {
-  const templateVars = { id: req.params.id, longURL: urlDatabase['b2xVn2'] }
+  const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id] }
   res.render('urls_show', templateVars)
 })
 
