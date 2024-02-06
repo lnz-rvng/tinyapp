e25bb38404885cafb56691a68d3420cf4cf4b2fe
Lance <lanceirvingmanzanal@icloud.com>
2024-02-06 14:18:37 -0700

    Added a user database

--- database.js
+++ database.js
@@ -3,4 +3,18 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 
-module.exports = { urlDatabase };
+const users = {
+  userRandomID: {
+    id: "userRandomID",
+    email: "user@example.com",
+    password: "purple-monkey-dinosaur",
+  },
+  user2RandomID: {
+    id: "user2RandomID",
+    email: "user2@example.com",
+    password: "dishwasher-funk",
+  },
+};
+
+
+module.exports = { urlDatabase, users };
