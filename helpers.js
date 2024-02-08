585855f76d7f495ca144c062ceee53ec32571e32
Lance <lanceirvingmanzanal@icloud.com>
2024-02-07 19:48:13 -0700

    Refactor the getUserByEmail helper function

--- helpers.js
+++ helpers.js
@@ -13,13 +13,12 @@ const generateRandomString = () => {
 };
 
 // used for authentication
-const getUserByEmail = (email) => {
+const getUserByEmail = (email, users) => {
   for (const userID in users) {
     if (users[userID].email === email) {
       return users[userID];
     }
   }
-  return null;
 };
 
 // Function to filter URLs for a specific user
