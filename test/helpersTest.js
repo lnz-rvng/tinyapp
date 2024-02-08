1e7fd905c6d0e3583e0b31a323f78faa27eaa22e
Lance <lanceirvingmanzanal@icloud.com>
2024-02-07 19:58:46 -0700

    Update test file

---
+++ test/helpersTest.js
@@ -0,0 +1,29 @@
+const { assert } = require('chai');
+
+const { getUserByEmail } = require('../helpers.js');
+
+const testUsers = {
+  "userRandomID": {
+    id: "userRandomID",
+    email: "user@example.com",
+    password: "purple-monkey-dinosaur"
+  },
+  "user2RandomID": {
+    id: "user2RandomID",
+    email: "user2@example.com",
+    password: "dishwasher-funk"
+  }
+};
+
+describe('getUserByEmail', function() {
+  it('should return a user with valid email', function() {
+    const user = getUserByEmail("user@example.com", testUsers);
+    const expectedUserID = "userRandomID";
+    assert.deepEqual(user.id, expectedUserID);
+  });
+
+  it('should return undefined when the email is not valid', () => {
+    const user = getUserByEmail("potato@potato.com", testUsers);
+    assert.isUndefined(user);
+  });
+});
