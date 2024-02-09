e93a2c1287dc147f2eda9d806aa6b61382aa3be9
Lance <lanceirvingmanzanal@icloud.com>
2024-02-08 17:32:41 -0700

    Update the databse

--- database.js
+++ database.js
@@ -13,12 +13,12 @@ const users = {
   userRandomID: {
     id: "userRandomID",
     email: "user@example.com",
-    password: "purple-monkey-dinosaur",
+    password: "$2a$10$5LUo1Or18uUK4DP0mHVHC.52ulxZCJBWyAfwirvNP9YJ9F49Zggu2", // purple-monkey-dinosaur
   },
   user2RandomID: {
     id: "user2RandomID",
     email: "user2@example.com",
-    password: "dishwasher-funk",
+    password: "$2a$10$pPCOP6gM3PP8zmkzLq6qPelBDu1.3nYXEOG2ANQVu8DP4.rQpQjfq", // dishwasher-funk
   },
 };
 
